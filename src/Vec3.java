cf037ffe8fceb7388575f1823cc8243946b4b4cd
Dworv <dwarvyt@gmail.com>
2023-05-14 10:30:49 -0700

    feat: distance method for vec3

--- src/Vec3.java
+++ src/Vec3.java
@@ -74,4 +74,9 @@ public class Vec3 {
     public double sum() {
         return x + y + z;
     }
+
+    public double distance(Vec3 other) {
+        double sum = Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2) + Math.pow(this.z - other.z, 2);
+        return Math.sqrt(sum);
+    }
 }
