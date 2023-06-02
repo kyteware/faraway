1dca693e250550381811bad22b75ac78457af564
Dworv YT <dwarvyt@gmail.com>
2023-06-02 14:54:01 -0700

    doc: add docs to draw util

--- src/main/java/com/dworv/faraway/Draw.java
+++ src/main/java/com/dworv/faraway/Draw.java
@@ -6,6 +6,29 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 public class Draw {
+    /**
+     * Draw an image to a file.
+     * <p>
+     * Pseudocode:
+     * <pre>
+     * <ol>
+     * <li>create a new BufferedImage</li>
+     * <li>for each pixel in the image:
+     * <li>set the pixel to the corresponding pixel in the pixels array</li>
+     * </ol>
+     * Example:
+     * <pre>
+     * Color[][] pixels = new Color[100][100];
+     * for (int i=0; i<100; i++) {
+     *     for (int j=0; j<100; j++) {
+     *         pixels[i][j] = new Color(1, 0, 0);
+     *     }
+     * }
+     * drawImage(pixels, "red.png");
+     * </pre>
+     * @param pixels the pixels to draw
+     * @param path the path to draw to
+     */
     public static void drawImage(Color[][] pixels, String path) {
         int width = pixels[0].length;
         int height = pixels.length;
