9ad2aed81b354d3e143601174507706efd508e4f
Dworv YT <dwarvyt@gmail.com>
2023-06-02 14:53:52 -0700

    doc: add docs to cube

--- src/main/java/com/dworv/faraway/Cube.java
+++ src/main/java/com/dworv/faraway/Cube.java
@@ -1,19 +1,54 @@
 package com.dworv.faraway;
 
-/*        +++
+/**
+ * Cube is used to represent a cube in a scene.
+ * <p>
+ * It is defined by a center point, a size, and a texture.
+ * <p>
+ * Diagram:
+ * <pre>
+ * top
  *     g
  *  f  |  h
  *  | e|  |
  *  | |c  |
  *  b |   d
  *    a
- * ---
+ * bottom
+ * </pre>
+ * @author github.com/dworv
  */
 class Cube extends Shape {
+    /**
+     * The center point of the cube.
+     * <p>
+     * This is a Vec3 object, which is a 3D vector with x, y, and z components.
+     */
     private Vec3 center;
+    /**
+     * The size of the cube.
+     * <p>
+     * This is a number that represents the size of the cube.
+     */
     private double size;
+    /**
+     * The texture of the cube.
+     * <p>
+     * This is a Texture object, which contains the texture of the cube.
+     */
     private Texture texture;
 
+    /**
+     * Create a new Cube.
+     * <p>
+     * Example:
+     * <pre>
+     * Cube cube = new Cube(new Vec3(0, 0, 0), 1, new Texture("textures/stone.png")); // cube at (0, 0, 0) with size 1 and texture "textures/stone.png"
+     * </pre>
+     * @param center the center point of the cube
+     * @param size the size of the cube
+     * @param texture the texture of the cube
+     */
     Cube(Vec3 center, double size, Texture texture) {
         this.center = center;
         this.size = size;
